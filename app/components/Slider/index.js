f5763339bbfb754dbf14e47f23a04131f737c2c8
Lukas Anderson <luke.anderson2010@hotmail.com>
2020-02-18 15:03:18 -0800

    lazy load implemented, refactored Slider, ArticleViewer now uses state hook

--- app/components/Slider/index.js
+++ app/components/Slider/index.js
@@ -10,8 +10,8 @@ import styled, { css } from 'styled-components';
 import arrowRight from 'images/icons8-chevron-right-64.png';
 import arrowLeft from 'images/icons8-chevron-left-64.png';
 import useWindowDimensions from 'utils/windowDimensions';
-import Img from 'components/Img';
-import Slide from '../Slide/Loadable';
+import Img from 'components';
+import Slide from '../Slide';
 import pictures from '../../images/all_images';
 
 const StyledSlider = styled.div`
